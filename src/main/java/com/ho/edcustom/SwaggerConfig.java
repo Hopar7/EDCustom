303982d48bf985fc43fa2247bac1d1b6bb4c9814
Hopar7 <mkiu7030@naver.com>
2024-06-30 17:15:11 +0900

    333
    333

--- src/main/java/com/ho/edcustom/SwaggerConfig.java
+++ src/main/java/com/ho/edcustom/SwaggerConfig.java
@@ -4,9 +4,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+    }
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
