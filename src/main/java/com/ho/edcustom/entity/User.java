07ef26ada6c93aa88600275454a7fe6343b8af4e
Hopar7 <mkiu7030@naver.com>
2024-07-02 23:13:51 +0900

    112311
    1123

--- src/main/java/com/ho/edcustom/entity/User.java
+++ src/main/java/com/ho/edcustom/entity/User.java
@@ -14,7 +14,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq",sequenceName = "user_sequence",allocationSize = 1)
     private Long id;
 
     @Column(unique = true)
