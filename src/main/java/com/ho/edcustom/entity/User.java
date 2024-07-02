a00cdcff57a300a20738b2a6944ccab472bb6e90
Hopar7 <mkiu7030@naver.com>
2024-07-02 22:53:36 +0900

    12312311
    123

--- src/main/java/com/ho/edcustom/entity/User.java
+++ src/main/java/com/ho/edcustom/entity/User.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
