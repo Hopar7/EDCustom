3f3c94d30cd8a5b1b0880bb1061de335de49f813
mkiu7030 <mkiu7030@naver.com>
2024-07-13 20:20:47 +0900

    1234
    1234

--- src/main/java/com/ho/edcustom/WebConfig.java
+++ src/main/java/com/ho/edcustom/WebConfig.java
@@ -2,6 +2,7 @@ package com.ho.edcustom;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final Interceptor interceptor;
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor)
