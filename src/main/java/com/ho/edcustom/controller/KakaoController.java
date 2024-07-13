dfddf731147d9923f892d75e8a1b48d0dec65ff6
Hopar7 <mkiu7030@naver.com>
2024-07-13 16:23:57 +0900

    1234
    1234

--- src/main/java/com/ho/edcustom/controller/KakaoController.java
+++ src/main/java/com/ho/edcustom/controller/KakaoController.java
@@ -11,9 +11,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class KakaoController {
     private final KakaoService kakaoService;
-    @GetMapping("/user/kakao/login")
-    public String kakaoLogin(@RequestBody KakaoRequestDTO DTO) throws JsonProcessingException {
-        return kakaoService.kakaoLogin(DTO.getCode());
-    }
+//    @GetMapping("/user/kakao/login")
+//    public String kakaoLogin(@RequestBody KakaoRequestDTO DTO) throws JsonProcessingException {
+//        return kakaoService.kakaoLogin(DTO.getCode()); //400에러
+//    }
+@GetMapping("/user/kakao/login")
+public String kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+    return kakaoService.kakaoLogin(code); //500에러 - 디버그 400에러
+    //근데 리다이랙트 엔드포인트 call back으로 바꾸면 200 성공함 왜인지 알아야함.
+    //Post,Get 일단 둘다됨
+}
 
 }
