31f7df2edbfe1c2179b90b02355ba2cef56c561a
Hopar7 <mkiu7030@naver.com>
2024-07-13 15:57:10 +0900

    123
    123

--- src/main/java/com/ho/edcustom/controller/KakaoController.java
+++ src/main/java/com/ho/edcustom/controller/KakaoController.java
@@ -4,17 +4,14 @@ import com.ho.edcustom.DTO.KakaoRequestDTO;
 import com.ho.edcustom.service.KakaoService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 public class KakaoController {
     private final KakaoService kakaoService;
-    @PostMapping("/user/kakao/login")
+    @GetMapping("/user/kakao/login")
     public String kakaoLogin(@RequestBody KakaoRequestDTO DTO) throws JsonProcessingException {
         return kakaoService.kakaoLogin(DTO.getCode());
     }
